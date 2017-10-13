84cb9e8b42eb2a26c971d3fd816f14e6382718fc
kolbrunhh <kolbrunh16@ru.is>
2017-10-13 21:01:37 +0000

    Can now add two numbers together and get their sum

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -4,6 +4,18 @@ public class Calculator
 {
 	public static int add(String text)
 	{
-		return 0;
+		if(text.equals(""))
+		{
+			return 0;
+		}
+		else
+		{
+			if(text.contains(","))
+			{
+				String numbers[] = text.split(",");
+				return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+			}
+			return 1;
+		}
 	}
 }
