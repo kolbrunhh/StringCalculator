9176fbc9fbbce1506cefd60812e375c901d6796c
kolbrunhh <kolbrunh16@ru.is>
2017-10-13 21:07:36 +0000

    Refactoring

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -13,9 +13,14 @@ public class Calculator
 			if(text.contains(","))
 			{
 				String numbers[] = text.split(",");
-				return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+				return toInt(numbers[0]) + toInt(numbers[1]);
 			}
 			return 1;
 		}
+	}
+
+	private static int toInt(String number)
+	{
+		return Integer.parseInt(number);
 	}
 }
